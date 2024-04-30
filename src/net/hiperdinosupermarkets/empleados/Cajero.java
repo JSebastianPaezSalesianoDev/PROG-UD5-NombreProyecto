1afcb7a987079defc3450f10fe85b96c6ccc505d
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-04-30 13:55:47 +0100

    feat(cajero) se creo la clase cajero con sus respectivos atributos, getters and setters

---
+++ src/net/hiperdinosupermarkets/empleados/Cajero.java
@@ -0,0 +1,23 @@
+package net.hiperdinosupermarkets.empleados;
+
+public class Cajero {
+    
+    private int numCaja;
+    private int numClientes;
+
+    
+    public int getNumCaja() {
+        return numCaja;
+    }
+    public void setNumCaja(int numCaja) {
+        this.numCaja = numCaja;
+    }
+    public int getNumClientes() {
+        return numClientes;
+    }
+    public void setNumClientes(int numClientes) {
+        this.numClientes = numClientes;
+    }
+   
+    
+}
