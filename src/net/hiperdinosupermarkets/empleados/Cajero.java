67b98707d8ba45be15cb39c84cef128ef32e5c81
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-04-30 14:09:23 +0100

    feat(cajero) se ha terminado el toString cajero

--- src/net/hiperdinosupermarkets/empleados/Cajero.java
+++ src/net/hiperdinosupermarkets/empleados/Cajero.java
@@ -1,10 +1,15 @@
 package net.hiperdinosupermarkets.empleados;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import net.hiperdinosupermarkets.clientes.Cliente;
+
 public class Cajero {
     
     private int numCaja;
     private int numClientes;
-
+    private Queue<Cliente> colaClientes = new LinkedList<>();
     
     public int getNumCaja() {
         return numCaja;
@@ -17,6 +22,20 @@ public class Cajero {
     }
     public void setNumClientes(int numClientes) {
         this.numClientes = numClientes;
+    }
+
+
+    @Override
+    public String toString() {
+        
+        String mensaje = "*Numero de caja: " + this.numCaja + " > :";
+        mensaje += "total clientes: < " + colaClientes.size() + " >";
+        mensaje += "Clientes en fila: ";
+         for (Cliente cliente : colaClientes) {
+            mensaje += "- " + cliente + "\n";
+        }
+
+        return mensaje;
     }
    
     
