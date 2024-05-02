eabaa545f70238a3b1ea4ac343d0ef1342522eda
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-05-02 20:12:32 +0100

    fix(ejecutable) se me perdio la rama xd

--- src/net/hiperdinosupermarkets/empleados/Cajero.java
+++ src/net/hiperdinosupermarkets/empleados/Cajero.java
@@ -2,24 +2,29 @@ package net.hiperdinosupermarkets.empleados;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
+
 import net.hiperdinosupermarkets.clientes.Cliente;
 
 public class Cajero {
-    
+
     private int numCaja;
     private int numClientes;
-    private boolean estadoCaja;
+    private boolean estadoCaja = false;
     private Queue<Cliente> colaClientes = new LinkedList<>();
-    
+
     public int getNumCaja() {
         return numCaja;
     }
+
     public void setNumCaja(int numCaja) {
         this.numCaja = numCaja;
     }
+
     public int getNumClientes() {
         return numClientes;
     }
+
     public void setNumClientes(int numClientes) {
         this.numClientes = numClientes;
     }
@@ -27,21 +32,36 @@ public class Cajero {
     public boolean isEstadoCaja() {
         return estadoCaja;
     }
+
     public void setEstadoCaja(boolean estadoCaja) {
         this.estadoCaja = estadoCaja;
     }
+
+    public void añadirClienteCola() {
+
+        Cliente cliente = new Cliente();
+
+        Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(12);
+
+        for (int i = 0; i < numeroAleatorio; i++) {
+            cliente.añadirProductoCesta();
+
+        }
+        colaClientes.add(cliente);
+    }
+
     @Override
     public String toString() {
-        
+
         String mensaje = "*Numero de caja: " + this.numCaja + " > :";
         mensaje += "total clientes: < " + colaClientes.size() + " >";
         mensaje += "Clientes en fila: ";
-         for (Cliente cliente : colaClientes) {
+        for (Cliente cliente : colaClientes) {
             mensaje += "- " + cliente + "\n";
         }
 
         return mensaje;
     }
-   
-    
+
 }
