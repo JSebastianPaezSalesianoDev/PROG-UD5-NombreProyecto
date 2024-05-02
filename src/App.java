7f71b2373f8ea0ec1e6fa0649424dd56bb855a29
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-05-02 20:16:09 +0100

    fixed(rdesarrollo) recupere lo que perdi  (creo)

--- src/App.java
+++ src/App.java
@@ -1,17 +1,40 @@
-import net.hiperdinosupermarkets.clientes.Cliente;
-import net.hiperdinosupermarkets.generador.GeneradorProductos;
+import java.util.Scanner;
+
+import net.hiperdinosupermarkets.empleados.Cajero;
 import net.hiperdinosupermarkets.utilidades.Menu;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(GeneradorProductos.obtenerProductoAleatorio());
+    public static void main(String[] args) {
 
-        Cliente cliente = new Cliente();
-        cliente.añadirProductoCesta();
-        cliente.añadirProductoCesta();
-        cliente.añadirProductoCesta();
-        System.out.println(cliente.toString());
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println(Menu.menu());
+
+        String opcionMenuString = scanner.nextLine();
+
+        int opcionMenu = Integer.parseInt(opcionMenuString);
+
+        boolean seguirPreguntando = true;
+
+        Cajero cajero = new Cajero();
+
+        while (seguirPreguntando) {
+
+            switch (opcionMenu) {
+
+                case 1:
+
+                    if (cajero.isEstadoCaja() == false) {
+                        cajero.setEstadoCaja(true);
+                        System.out.println("Caja abierta");
+                    }
+                    break;
+                case 2:
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
--- src/net/hiperdinosupermarkets/empleados/Cajero.java
+++ src/net/hiperdinosupermarkets/empleados/Cajero.java
@@ -8,28 +8,22 @@ import net.hiperdinosupermarkets.clientes.Cliente;
 
 public class Cajero {
 
-
     private int numCaja;
     private int numClientes;
     private boolean estadoCaja = false;
-    private boolean estadoCaja = false;
     private Queue<Cliente> colaClientes = new LinkedList<>();
-
 
     public int getNumCaja() {
         return numCaja;
     }
 
-
     public void setNumCaja(int numCaja) {
         this.numCaja = numCaja;
     }
 
-
     public int getNumClientes() {
         return numClientes;
     }
-
 
     public void setNumClientes(int numClientes) {
         this.numClientes = numClientes;
@@ -38,7 +32,6 @@ public class Cajero {
     public boolean isEstadoCaja() {
         return estadoCaja;
     }
-
 
     public void setEstadoCaja(boolean estadoCaja) {
         this.estadoCaja = estadoCaja;
@@ -61,17 +54,14 @@ public class Cajero {
     @Override
     public String toString() {
 
-
         String mensaje = "*Numero de caja: " + this.numCaja + " > :";
         mensaje += "total clientes: < " + colaClientes.size() + " >";
         mensaje += "Clientes en fila: ";
         for (Cliente cliente : colaClientes) {
-        for (Cliente cliente : colaClientes) {
             mensaje += "- " + cliente + "\n";
-        }
 
+        }
         return mensaje;
     }
-
 
 }
